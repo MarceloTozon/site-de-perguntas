e91d7b88559582eed0fb6fbd4f87d7def1e44fcb
Marcelo Tozon <tozonmarcelo@gmail.com>
2021-12-10 17:26:26 -0300

    atualização

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,6 +1,18 @@
-module.exports = function modal(){
-    function open(){}
-    function close(){}
+export default function modal(){
+    
+    const modalWrapper =  document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.button.cancel')
+
+    cancelButton.addEventListener("click", close)
+
+    function open(){
+        //funcionalidade de atribuir a classe active para a modal
+       modalWrapper.classList.add("active")
+    }
+    function close(){
+       //funcionalidade de remover a classe active da modal
+      modalWrapper.classList.remove("active")
+    }   
 
     return{
         open,
