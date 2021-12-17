4f72a150707fd96cc46b20344caf082da28db97d
Marcelo Tozon <tozonmarcelo@gmail.com>
2021-12-17 18:03:26 -0300

    atualizaçao

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -21,13 +21,20 @@ checkButtons.forEach(button => {
 
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
-deleteButton.forEach(button => {
+deleteButton.forEach(button => { 
     button.addEventListener("click", (event) => handleClick(event ,  false))
 })
 
 function handleClick(event, check = true){
     event.preventDefault()
     const text = check ? "Marcar como lida " : "Excluir "
+    const slug = check ? "check" : "delete"
+
+    const roomId = document.querySelector("#room-id").dataset.id
+    const questionId = event.target.dataset.id
+
+    const form = document.querySelector(".modal form")
+    form.setAttribute("action", `/room/${roomId}/${questionId}/${slug}`)
 
     modalTitle.innerHTML= `${text} esta pergunta`
     modalDescription.innerHTML = `Tem certeza que deseja ${text.toLowerCase()} esta pergunta?` 
--- src/route.js
+++ src/route.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const res = require('express/lib/response')
+const QuestionController = require ('./controllers/QuestionController')
 
 
 
@@ -7,12 +7,14 @@ const route = express.Router()
 
 
 
-route.get('/', (req, res) => res.render("index"))
+route.get('/', (req, res) => res.render("index", {page: 'enter-room'}))
+route.get('/create-pass',(req,res) => res.render("index", {page: 'create-pass'}))
 route.get('/room',(req,res) => res.render("room"))
-route.get('/create-pass',(req,res) => res.render("create-pass"))
 
 
-//route.post('/room/:room/:question/:action')
+
+//formato que o formularia de dentro da modal tem que passar a informaçao
+route.post('/room/:room/:question/:action', QuestionController.index )
 
 module.exports = route
 
